cb121b5be40ed43d0fd46db83f28d0448b5bdae3
Muhammad Tarek <tarek3529@gmail.com>
2020-05-17 23:58:35 +0200

    Updates

---
+++ precache-manifest.2e1b379fd2d278763c6d55a713ed0270.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a56cc9fa800702416b0e5aa3c15e74f",
+    "url": "/github-users/index.html"
+  },
+  {
+    "revision": "81cbefa6553b90e8d860",
+    "url": "/github-users/static/css/2.9de105d4.chunk.css"
+  },
+  {
+    "revision": "0c2e51773a086e7b6a20",
+    "url": "/github-users/static/css/main.9f56629f.chunk.css"
+  },
+  {
+    "revision": "81cbefa6553b90e8d860",
+    "url": "/github-users/static/js/2.9764c142.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-users/static/js/2.9764c142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2e51773a086e7b6a20",
+    "url": "/github-users/static/js/main.291d17c2.chunk.js"
+  },
+  {
+    "revision": "8fea2bf6899344e6e66b",
+    "url": "/github-users/static/js/runtime-main.56d6e6c9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github-users/precache-manifest.bb0fb1e852288da5b74cb5f3d03d22f7.js"
+  "/github-users/precache-manifest.2e1b379fd2d278763c6d55a713ed0270.js"
 );
 
 self.addEventListener('message', (event) => {
